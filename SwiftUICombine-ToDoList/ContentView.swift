2aeaa34a5b2311f68aa7cd1c568144555ff79afb
poga07 <chris@168money.com.tw>
2025-02-10 11:19:16 +0800

    Initial Commit

---
+++ SwiftUICombine-ToDoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUICombine-ToDoList
+//
+//  Created by 黃柏嘉 on 2025/02/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUICombine-ToDoList/SwiftUICombine_ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombine_ToDoListApp.swift
+//  SwiftUICombine-ToDoList
+//
+//  Created by 黃柏嘉 on 2025/02/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombine_ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
